09f2f25334865c9adb192dc43eb145893ba099e3
Jadir Junior da Silva Alves <jadir.alves@foa.org.br>
2021-07-30 16:59:31 -0300

    Desenvolvimento

--- InventoryControlTRD.CrossCutting/Extensions/Ioc/IocInjectorExtension.cs
+++ InventoryControlTRD.CrossCutting/Extensions/Ioc/IocInjectorExtension.cs
@@ -2,36 +2,44 @@
 using InventoryControlTRD.Domain.Core.Interfaces.Repositories;
 using InventoryControlTRD.Domain.Core.Interfaces.Services;
 using InventoryControlTRD.Domain.Services;
+using InventoryControlTRD.Infrastructure.Data.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace InventoryControlTRD.CrossCutting.Extensions.Ioc
 {
     public static class IocInjectorExtension
     {
-        public static void AddMapper(this IServiceCollection services)
+        private static void AddMapper(this IServiceCollection services)
         {
             services.AddSingleton<IMapper, Mapper>();
         }
 
-        //public static void AddProductService(this IServiceCollection services)
-        //{
-        //    services.AddScoped<IProductRepository, ProductRepository>();
-        //    services.AddScoped<IProductService, ProductService>();
-        //}
-        //public static void AddInventoryService(this IServiceCollection services)
-        //{
-        //    services.AddScoped<IInventoryRepository, InventoryRepository>();
-        //    services.AddScoped<IProductService, ProductService>();
-        //}
-        //public static void AddProductService(this IServiceCollection services)
-        //{
-        //    services.AddScoped<IProductRepository, IProductRepository>();
-        //    services.AddScoped<IProductService, ProductService>();
-        //}
-        //public static void AddProductService(this IServiceCollection services)
-        //{
-        //    services.AddScoped<IProductRepository, IProductRepository>();
-        //    services.AddScoped<IProductService, ProductService>();
-        //}
+        public static void AddEssentialsServices(IServiceCollection services)
+        {
+            AddMapper(services);
+            AddProductService(services);
+            AddInventoryService(services);
+        }
+
+        public static void AddProductService(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+        }
+        public static void AddInventoryService(this IServiceCollection services)
+        {
+            services.AddScoped<IInventoryRepository, InventoryRepository>();
+            services.AddScoped<IProductService, ProductService>();
+        }
+        public static void AddMovimentService(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, IProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+        }
+        public static void AddUsertService(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, IProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+        }
     }
 }
---
+++ InventoryControlTRD.Infrastructure/Data/Repositories/ProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using InventoryControlTRD.Domain.Core.Interfaces.Repositories;
+using InventoryControlTRD.Domain.Models;
+using InventoryControlTRD.Infrastructure.Data.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryControlTRD.Infrastructure.Data.Repositories
+{
+    public class ProductRepository : IBaseRepository<Product>, IProductRepository
+    {
+        private IDataCore<Product> _data;
+
+        /*
+          public Guid ProductId { get; set; }
+        public int Ammout { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+         */
+        public ProductRepository(IDataCore<Product> data)
+        {
+            _data = data;
+        }
+        public void AddAsync(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAsync(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAsync(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
